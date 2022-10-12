c56813e0c2c978d34c9cd62f9e620c467c388e98
david sureway <nwachukwudavid100@gmail.com>
2022-10-12 17:14:11 +0100

    Django Model- Applying CRUD

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400, null=True)
+    age = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    title = models.CharField(max_length=400)
+    date_released = models.DateTimeField(default=datetime.now, blank=True)
+    #date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)  # --one artiste can have multiple song --one to many
+    
+    def __str__(self):
+        return self.title
+    
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=400)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)  # --- one song can have multiple lyrics --- one to many
+    def __str__(self):
+        return self.content
+    
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,6 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# def index(request):
+#     return HttpResponse('Hello world, this is your home')
+
+
 def index(request):
-    return HttpResponse('hello world')
+    return render(request, 'home.html', 
+                  {'header': 'Welcome',
+                   'name': 'david'})
